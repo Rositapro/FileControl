cb36062d6e6f77cc137c0dc0870300fc55fd9072
Rosalinda Cedillo <rosalindacedillo2017@gmail.com>
2024-05-01 22:24:26 -0600

    Agregar archivos de proyecto.

---
+++ FileControl/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FileControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSaveAsWord = new Button();
+            btnSaveTextFile = new Button();
+            txtContent = new TextBox();
+            btnOpenTextFile = new Button();
+            txtProcessFileNumber = new TextBox();
+            btnProcessFolder = new Button();
+            txtFolder = new TextBox();
+            btnSelectFolder = new Button();
+            SuspendLayout();
+            // 
+            // btnSaveAsWord
+            // 
+            btnSaveAsWord.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSaveAsWord.Location = new Point(520, 118);
+            btnSaveAsWord.Name = "btnSaveAsWord";
+            btnSaveAsWord.Size = new Size(182, 45);
+            btnSaveAsWord.TabIndex = 15;
+            btnSaveAsWord.Text = "Save As Word";
+            btnSaveAsWord.UseVisualStyleBackColor = true;
+            btnSaveAsWord.Click += btnSaveAsWord_Click;
+            // 
+            // btnSaveTextFile
+            // 
+            btnSaveTextFile.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSaveTextFile.Location = new Point(297, 118);
+            btnSaveTextFile.Name = "btnSaveTextFile";
+            btnSaveTextFile.Size = new Size(182, 45);
+            btnSaveTextFile.TabIndex = 14;
+            btnSaveTextFile.Text = "Save Text File";
+            btnSaveTextFile.UseVisualStyleBackColor = true;
+            btnSaveTextFile.Click += btnSaveTextFile_Click;
+            // 
+            // txtContent
+            // 
+            txtContent.Font = new Font("Segoe UI", 20F);
+            txtContent.Location = new Point(29, 196);
+            txtContent.Multiline = true;
+            txtContent.Name = "txtContent";
+            txtContent.ScrollBars = ScrollBars.Both;
+            txtContent.Size = new Size(650, 215);
+            txtContent.TabIndex = 13;
+            // 
+            // btnOpenTextFile
+            // 
+            btnOpenTextFile.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnOpenTextFile.Location = new Point(29, 118);
+            btnOpenTextFile.Name = "btnOpenTextFile";
+            btnOpenTextFile.Size = new Size(125, 40);
+            btnOpenTextFile.TabIndex = 12;
+            btnOpenTextFile.Text = "Open Text File";
+            btnOpenTextFile.UseVisualStyleBackColor = true;
+            btnOpenTextFile.Click += btnOpenTextFile_Click;
+            // 
+            // txtProcessFileNumber
+            // 
+            txtProcessFileNumber.Font = new Font("Segoe UI", 20F);
+            txtProcessFileNumber.Location = new Point(547, 53);
+            txtProcessFileNumber.Name = "txtProcessFileNumber";
+            txtProcessFileNumber.ReadOnly = true;
+            txtProcessFileNumber.Size = new Size(225, 43);
+            txtProcessFileNumber.TabIndex = 11;
+            // 
+            // btnProcessFolder
+            // 
+            btnProcessFolder.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnProcessFolder.Location = new Point(370, 39);
+            btnProcessFolder.Name = "btnProcessFolder";
+            btnProcessFolder.Size = new Size(158, 57);
+            btnProcessFolder.TabIndex = 10;
+            btnProcessFolder.Text = "Process Folder";
+            btnProcessFolder.UseVisualStyleBackColor = true;
+            btnProcessFolder.Click += btnProcessFolder_Click;
+            // 
+            // txtFolder
+            // 
+            txtFolder.Font = new Font("Segoe UI", 20F);
+            txtFolder.Location = new Point(176, 53);
+            txtFolder.Name = "txtFolder";
+            txtFolder.ReadOnly = true;
+            txtFolder.Size = new Size(159, 43);
+            txtFolder.TabIndex = 9;
+            // 
+            // btnSelectFolder
+            // 
+            btnSelectFolder.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSelectFolder.Location = new Point(29, 51);
+            btnSelectFolder.Name = "btnSelectFolder";
+            btnSelectFolder.Size = new Size(125, 45);
+            btnSelectFolder.TabIndex = 8;
+            btnSelectFolder.Text = "Select Folder";
+            btnSelectFolder.UseVisualStyleBackColor = true;
+            btnSelectFolder.Click += btnSelectFolder_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSaveAsWord);
+            Controls.Add(btnSaveTextFile);
+            Controls.Add(txtContent);
+            Controls.Add(btnOpenTextFile);
+            Controls.Add(txtProcessFileNumber);
+            Controls.Add(btnProcessFolder);
+            Controls.Add(txtFolder);
+            Controls.Add(btnSelectFolder);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSaveAsWord;
+        private Button btnSaveTextFile;
+        private TextBox txtContent;
+        private Button btnOpenTextFile;
+        private TextBox txtProcessFileNumber;
+        private Button btnProcessFolder;
+        private TextBox txtFolder;
+        private Button btnSelectFolder;
+    }
+}
---
+++ FileControl/Form1.cs
@@ -0,0 +1,234 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+
+namespace FileControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            //Open a Folder browser dialog to select the folder
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Select the folder";
+            folderBrowserDialog.ShowNewFolderButton = false;
+            folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+            //if the user select a folder, set the path to the textbox
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            txtFolder.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void btnProcessFolder_Click(object sender, EventArgs e)
+        {
+            string[] filesInFolder = Directory.GetFiles(txtFolder.Text);
+            //show in txtProcessFileNumber the number of items in the filesInFolder Array
+            txtProcessFileNumber.Text = filesInFolder.Length + " file(s) Processed";
+            foreach (string file in filesInFolder)
+            {
+                //Get the file name
+                string fileName = Path.GetFileName(file);
+                //Get the file extension
+                string fileExtension = Path.GetExtension(file);
+                //sort the file by extension using a switch
+                switch (fileExtension)
+                {
+                    case ".txt":
+                    case ".xlsx":
+                    case ".docx":
+                    case ".pptx":
+                        //Move the file to the folder to a docs
+                        if (!Directory.Exists(txtFolder.Text + "\\Docs"))
+                        {
+                            Directory.CreateDirectory(txtFolder.Text + "\\Docs");
+                        }
+                        File.Move(file, txtFolder.Text + "\\Docs\\" + fileName);
+                        break;
+                    case ".jpg":
+                    case ".jpeg":
+                    case ".png":
+                        //Move the file to the  folder if the folder exist else create the folder
+                        if (!Directory.Exists(txtFolder.Text + "\\Pics"))
+                        {
+                            Directory.CreateDirectory(txtFolder.Text + "\\Pics");
+                        }
+                        File.Move(file, txtFolder.Text + "\\Pics\\" + fileName);
+                        break;
+
+                    default:
+                        //Move the file to the other folder
+                        if (!Directory.Exists(txtFolder.Text + "\\other"))
+                        {
+                            Directory.CreateDirectory(txtFolder.Text + "\\other");
+                        }
+                        File.Move(file, txtFolder.Text + "\\other\\" + fileName);
+                        break;
+                }
+
+            }
+        }
+
+        private void btnOpenTextFile_Click(object sender, EventArgs e)
+        {
+            //Open a file dialog to select the file
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files|*.txt";
+            dialog.Title = "Select a Text File";
+            //if the user select a file, set the path to the textbox
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            //Read the file and show the content in the txtContent textbox
+            txtContent.Text = File.ReadAllText(dialog.FileName);
+        }
+
+        private void btnSaveTextFile_Click(object sender, EventArgs e)
+        {
+            //save the content of the txtContent textbox to a new file
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text Files|*.txt";
+            dialog.Title = "Save the Text File";
+            //if the user select a file, set the path to the textbox
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            //Write the content of the txtContent textbox to the file
+            File.WriteAllText(dialog.FileName, txtContent.Text);
+        }
+
+        private void btnSaveAsWord_Click(object sender, EventArgs e)
+        {
+            //save the content of the txtContent textbox to a new file
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text Files|*.docx";
+            dialog.Title = "Save the Text File";
+            //if the user select a file, set the path to the textbox
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            //Write the content of the txtContent textbox to the file
+            File.WriteAllText(dialog.FileName, txtContent.Text);
+        }
+
+
+        //private void btnSelectFolder_Click(object sender, EventArgs e)
+        //{
+        //    //Open a Folder browser dialog to select the folder
+        //    FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+        //    folderBrowserDialog.Description = "Select the folder";
+        //    folderBrowserDialog.ShowNewFolderButton = false;
+        //    folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+        //    //if the user select a folder, set the path to the textbox
+        //    if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+        //    {
+        //        return;
+        //    }
+        //    txtFolder.Text = folderBrowserDialog.SelectedPath;
+        //}
+
+        //private void btnProcessFolder_Click(object sender, EventArgs e)
+        //{
+        //    string[] filesInFolder = Directory.GetFiles(txtFolder.Text);
+        //    //show in txtProcessFileNumber the number of items in the filesInFolder Array
+        //    txtProcessFileNumber.Text = filesInFolder.Length + " file(s) Processed";
+        //    foreach (string file in filesInFolder)
+        //    {
+        //        //Get the file name
+        //        string fileName = Path.GetFileName(file);
+        //        //Get the file extension
+        //        string fileExtension = Path.GetExtension(file);
+        //        //sort the file by extension using a switch
+        //        switch (fileExtension)
+        //        {
+        //            case ".txt":
+        //            case ".xlsx":
+        //            case ".docx":
+        //            case ".pptx":
+        //                //Move the file to the folder to a docs
+        //                if (!Directory.Exists(txtFolder.Text + "\\Docs"))
+        //                {
+        //                    Directory.CreateDirectory(txtFolder.Text + "\\Docs");
+        //                }
+        //                File.Move(file, txtFolder.Text + "\\Docs\\" + fileName);
+        //                break;
+        //            case ".jpg":
+        //            case ".jpeg":
+        //            case ".png":
+        //                //Move the file to the  folder if the folder exist else create the folder
+        //                if (!Directory.Exists(txtFolder.Text + "\\Pics"))
+        //                {
+        //                    Directory.CreateDirectory(txtFolder.Text + "\\Pics");
+        //                }
+        //                File.Move(file, txtFolder.Text + "\\Pics\\" + fileName);
+        //                break;
+
+        //            default:
+        //                //Move the file to the other folder
+        //                if (!Directory.Exists(txtFolder.Text + "\\other"))
+        //                {
+        //                    Directory.CreateDirectory(txtFolder.Text + "\\other");
+        //                }
+        //                File.Move(file, txtFolder.Text + "\\other\\" + fileName);
+        //                break;
+        //        }
+        //    }
+        //}
+
+        //private void btnOpenTextFile_Click(object sender, EventArgs e)
+        //{
+        //    //Open a file dialog to select the file
+        //    OpenFileDialog dialog = new OpenFileDialog();
+        //    dialog.Filter = "Text Files|*.txt";
+        //    dialog.Title = "Select a Text File";
+        //    //if the user select a file, set the path to the textbox
+        //    if (dialog.ShowDialog() != DialogResult.OK)
+        //    {
+        //        return;
+        //    }
+        //    //Read the file and show the content in the txtContent textbox
+        //    txtContent.Text = File.ReadAllText(dialog.FileName);
+        //}
+
+        //private void btnSaveTextFile_Click(object sender, EventArgs e)
+        //{
+        //    //save the content of the txtContent textbox to a new file
+        //    SaveFileDialog dialog = new SaveFileDialog();
+        //    dialog.Filter = "Text Files|*.txt";
+        //    dialog.Title = "Save the Text File";
+        //    //if the user select a file, set the path to the textbox
+        //    if (dialog.ShowDialog() != DialogResult.OK)
+        //    {
+        //        return;
+        //    }
+        //    //Write the content of the txtContent textbox to the file
+        //    File.WriteAllText(dialog.FileName, txtContent.Text);
+        //}
+
+        //private void btnSaveAsWord_Click(object sender, EventArgs e)
+        //{
+        //    //save the content of the txtContent textbox to a new file
+        //    SaveFileDialog dialog = new SaveFileDialog();
+        //    dialog.Filter = "Text Files|*.docx";
+        //    dialog.Title = "Save the Text File";
+        //    //if the user select a file, set the path to the textbox
+        //    if (dialog.ShowDialog() != DialogResult.OK)
+        //    {
+        //        return;
+        //    }
+        //    //Write the content of the txtContent textbox to the file
+        //    File.WriteAllText(dialog.FileName, txtContent.Text);
+        //}
+    }
+}
